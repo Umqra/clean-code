fa9ed45ca949d492f9a52037c45f16b93ebc3c8b
Umqra <golovoiapklavu@gmail.com>
2016-11-23 01:47:55 +0500

    Add template option in CLI

--- Markdown/Markdown.Cli/CliOptions.cs
+++ Markdown/Markdown.Cli/CliOptions.cs
@@ -7,13 +7,34 @@ namespace Markdown.Cli
         public string InputFilename { get; set; }
         public string OutputFilename { get; set; }
         public string BaseUrl { get; set; }
+        public string HtmlFilename { get; set; }
+        public string InjectedHtmlElement { get; set; }
 
         public CliOptions TryInitialize()
         {
             TryInitializeInputFile();
             TryInitializeOutputFile();
-
+            TryInitializeHtmlFile();
             return this;
+        }
+
+        private void TryInitializeHtmlFile()
+        {
+            if (HtmlFilename != null)
+            {
+                if (InjectedHtmlElement == null)
+                    throw new ArgumentException(
+                        "InjectedField must be specified if used HtmlFilename. It can be specified with --inject_el option"
+                    );
+                try
+                {
+                    FileExtensions.TryGetReadAccess(HtmlFilename);
+                }
+                catch (Exception exception)
+                {
+                    throw new ArgumentException($"Can't read from html file {HtmlFilename}", exception);
+                }
+            }
         }
 
         private void TryInitializeInputFile()
--- Markdown/Markdown.Cli/EntryPoint.cs
+++ Markdown/Markdown.Cli/EntryPoint.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using AngleSharp.Parser.Html;
 using Fclp;
 using Markdown.Parsing;
 using Markdown.Parsing.Tokenizer;
@@ -54,19 +55,41 @@ namespace Markdown.Cli
             var markdownMarkup = File.ReadAllText(options.InputFilename);
             MarkdownToHtmlRenderer markdownToHtmlRenderer = GetRenderer(options);
             var htmlMarkup = markdownToHtmlRenderer.Render(markdownMarkup);
-            File.WriteAllText(options.OutputFilename, htmlMarkup);
+
+            WriteResult(options, htmlMarkup);
+        }
+
+        private static void WriteResult(CliOptions options, string htmlMarkup)
+        {
+            if (options.HtmlFilename == null)
+                File.WriteAllText(options.OutputFilename, htmlMarkup);
+            else
+            {
+                var templateDom = new HtmlParser().Parse(File.OpenRead(options.HtmlFilename));
+                Console.WriteLine(templateDom.QuerySelector(options.InjectedHtmlElement).OuterHtml);
+                templateDom.QuerySelector(options.InjectedHtmlElement).InnerHtml = htmlMarkup;
+
+                File.WriteAllText(options.OutputFilename, templateDom.DocumentElement.OuterHtml);
+            }
+        }
+
+        private static INodeRenderer GetNodeRenderer(CliOptions options)
+        {
+            return new NodeHtmlRenderer(new HtmlRenderContext(new NodeToHtmlEntityConverter()));
         }
 
         private static MarkdownToHtmlRenderer GetRenderer(CliOptions options)
         {
-            var renderer = new MarkdownToHtmlRenderer(
+            var nodeRenderer = GetNodeRenderer(options);
+
+            var htmlRenderer = new MarkdownToHtmlRenderer(
                 new MarkdownParser(),
                 new MarkdownTokenizerFactory(),
-                new NodeHtmlRenderer(new HtmlRenderContext(new NodeToHtmlEntityConverter()))
+                nodeRenderer
             );
             if (options.BaseUrl != null)
-                renderer = renderer.WithModificators(new TransformTreeVisitor(new BaseUrlTransformer(options.BaseUrl)));
-            return renderer;
+                htmlRenderer = htmlRenderer.WithModificators(new TransformTreeVisitor(new BaseUrlTransformer(options.BaseUrl)));
+            return htmlRenderer;
         }
 
         private static FluentCommandLineParser<CliOptions> ConfigureParser()
@@ -88,6 +111,16 @@ namespace Markdown.Cli
                 .Setup(arg => arg.BaseUrl)
                 .As("base_url")
                 .WithDescription("Base url for relative links");
+
+            parser
+                .Setup(arg => arg.HtmlFilename)
+                .As("html_file")
+                .WithDescription("HTML template file when generated markup will be injected");
+
+            parser
+                .Setup(arg => arg.InjectedHtmlElement)
+                .As("inject_el")
+                .WithDescription(@"Element in HTML DOM in which will be injected generated markup. You can use well-known css-selectors for specifying needed element. For example: --inject_el #markdown, --inject_el body, --inject_el .markdown_class");
 
             parser.SetupHelp("h", "help", "?").Callback(text => Console.WriteLine(text));
             return parser;
